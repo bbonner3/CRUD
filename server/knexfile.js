371e5d85bdc8cc400653d7b59d27d8e5b6d5e6bd
bbonner3 <branden.bonner@gmail.com>
2020-10-09 23:29:10 -0400

    Recommitting - Working on Update

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,8 +3,42 @@
 module.exports = {
 
   development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+  staging: {
     client: 'postgresql',
-    connection: 'postgres://localhost/my_life'
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
 
 };
--- server/migrations/20201007200326_todo.js
+++ server/migrations/20201007200326_todo.js
@@ -3,17 +3,18 @@
 // description - text
 // done = boolean
 // date - datetime
-exports.up = function(knex) {
-  return knex.schema.createTable('todo', (table) => {
-    table.increments();
-    table.text('title').notNullable();
-    table.integer('priority').notNullable();
-    table.text('description');
-    table.boolean('done').defaultTo(false).notNullable();
-    table.datetime('date').notNullable();
-  });
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('todo', (table) => {
+        table.increments();
+        table.text('title').notNullable();
+        table.integer('priority').notNullable();
+        table.text('description');
+        table.boolean('done').defaultTo(false).notNullable();
+        table.dateTime('date').notNullable();
+    });
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTable('todo');
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('todo');
 };
